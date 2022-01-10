8e691d024fa80d41eb941bf22936b7f1e1af1c5f
ars75061 <74510295+ars75061@users.noreply.github.com>
2022-01-10 14:31:13 -0600

    Update main.dart

--- arkadia_app/lib/main.dart
+++ arkadia_app/lib/main.dart
@@ -18,22 +18,24 @@ class ArkadiaAdministrationApp extends StatelessWidget {
     return MaterialApp(
       title: 'Arkadia Administration',
       //everything inside of this ThemeData object were auto-generated and the comments may be useful later
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: Colors.indigo[800],
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      // theme: ThemeData(
+      //   // This is the theme of your application.
+      //   //
+      //   // Try running your application with "flutter run". You'll see the
+      //   // application has a blue toolbar. Then, without quitting the app, try
+      //   // changing the primarySwatch below to Colors.green and then invoke
+      //   // "hot reload" (press "r" in the console where you ran "flutter run",
+      //   // or simply save your changes to "hot reload" in a Flutter IDE).
+      //   // Notice that the counter didn't reset back to zero; the application
+      //   // is not restarted.
+      //   primaryColor: Colors.indigo[800],
+      //   // This makes the visual density adapt to the platform that you run
+      //   // the app on. For desktop platforms, the controls will be smaller and
+      //   // closer together (more dense) than on mobile platforms.
+      //   visualDensity: VisualDensity.adaptivePlatformDensity,
+      // ),
+      theme: ThemeData
+          .dark(), //dark theme data, will synch based on system settings
       //set "home" point as a GameCabinets widget, shown below
       home: GameCabinets(),
     );
